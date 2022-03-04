398ad413e14e52df8d930ac9176b73857fc53005
thdal <36514056+thdal@users.noreply.github.com>
2022-03-04 18:47:14 +0100

    up name sql scripts

--- sql/Atelier2JenkinsDML.sql
+++ sql/Atelier2JenkinsDML.sql
@@ -1,27 +1,52 @@
-SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS contract;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS product;
+INSERT INTO contract (id, type, margin) VALUES (1,'contrat 1', 5);
+INSERT INTO contract (id, type, margin) VALUES (2,'contrat 2', 10);
+INSERT INTO contract (id, type, margin) VALUES (3,'contrat 3', 15);
+INSERT INTO contract (id, type, margin) VALUES (4,'contrat 4', 20);
+INSERT INTO contract (id, type, margin) VALUES (5,'contrat 5', 25);
 
-SET FOREIGN_KEY_CHECKS = 1;
+drop procedure if exists load_customer;
 
-CREATE TABLE IF NOT EXISTS `contract` (
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    type varchar(255) NOT NULL,
-    margin int
-    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+delimiter #
+create procedure load_customer()
+begin
 
-CREATE TABLE IF NOT EXISTS `customer` (
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    contract_id int NOT NULL,
-    FOREIGN KEY (contract_id) REFERENCES contract(id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+declare v_max int unsigned default 26;
+declare v_counter int unsigned default 1;
 
-CREATE TABLE IF NOT EXISTS `product` (
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    price float
-    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+truncate table customer;
+start transaction;
+while v_counter < v_max do
+    insert into customer (username, email, password, contract_id) values
+    (CONCAT('customer', v_counter), CONCAT('customer', v_counter, '@epsi.fr'), 'password123',
+    ( select FLOOR( RAND() * (6-1) + 1)) );
+    set v_counter=v_counter+1;
+end while;
+commit;
+end #
+
+delimiter ;
+
+call load_customer();
+
+drop procedure if exists load_product;
+
+delimiter #
+create procedure load_product()
+begin
+
+declare v_max int unsigned default 11;
+declare v_counter int unsigned default 1;
+
+truncate table product;
+start transaction;
+while v_counter < v_max do
+    insert into product (name, price) values 
+    (CONCAT('product', v_counter), ( select FLOOR( RAND() * (500 - 51) + 50))  );
+    set v_counter=v_counter+1;
+end while;
+commit;
+end #
+
+delimiter ;
+
+call load_product();
